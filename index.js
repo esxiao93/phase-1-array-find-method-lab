04785cb0785b2883333fe60c95d985f1fb7cfece
Eric Xiao <es.xiao93@gmail.com>
2022-04-14 11:41:08 -0400

    commit tree 89db4eaa8990985de566904fa08985701fdbcfd6

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// code your solution here
+//ARRAY FIND METHOD
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+//Write function called superbowlWin()
+function superbowlWin(element) {
+  return(element.result === "W")  
+}
+record.find(superbowlWin);
+
+
+
+
+// const superbowlWin = (record) => {
+//     const result = record.find(record => record.result === "W");
+//     if (result === "W"){
+//     return result.year;
+//     } else 
+//     return undefined;
+// }
+
+//The function should receive 1 argument, an Array of JavaScript Objects
+//Each object has two properties: year and result
+//It should use find() to test each Object to see if the result is "W" — a win!
+//It should return the year when the win occurred (if it occurred at all!)
+//If no win is found, it should return, sadly, undefined
+
+
+/*
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+  }
+  */
